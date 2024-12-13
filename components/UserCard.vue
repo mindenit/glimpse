ed108c1fc1cb3e8a61cd12c30bac2f66de3eb233
Plotkin <Plotkin2016@gmail.com>
2024-12-13 21:45:53 +0200

    feat: add users page and UserCard component

---
+++ components/UserCard.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { Avatar, Text } from '@mindenit/ui'
+import TheStar from './ui/TheStar.vue'
+defineProps({
+  name: String,
+  username: String,
+  avatarUrl: String,
+  description: String,
+  stars: {
+    type: Number,
+    default: 0,
+  },
+})
+</script>
+
+<template>
+  <div
+    class="h-full w-full rounded-3xl border border-fiord-300 bg-fiord-100 p-4 px-4 py-4 text-black shadow-lg dark:border-fiord-700 dark:bg-fiord-900 dark:text-white sm:px-6 sm:py-6"
+  >
+    <div class="flex items-start justify-between gap-4 pb-2 sm:gap-3">
+      <div class="flex min-w-0 gap-3 text-left">
+        <Avatar
+          size="48px"
+          :url="avatarUrl || '/default-avatar.png'"
+          :alt="`${name} Avatar`"
+          class="h-12 w-12 sm:h-16 sm:w-16"
+        />
+        <div class="">
+          <Text size="large" class="text-lg font-semibold sm:text-xl">
+            {{ name }}
+          </Text>
+          <Text
+            size="small"
+            class="text-sm font-semibold text-gray-500 sm:text-base"
+          >
+            {{ username }}
+          </Text>
+        </div>
+      </div>
+      <div class="flex-shrink-0">
+        <TheStar :model-value="stars" :max-stars="5" />
+      </div>
+    </div>
+
+    <Text
+      size="small"
+      class="line-clamp-3 w-full overflow-hidden overflow-ellipsis whitespace-normal text-left text-sm text-gray-700 sm:text-base"
+    >
+      {{ description }}
+    </Text>
+  </div>
+</template>
---
+++ pages/users.vue
@@ -0,0 +1,87 @@
+<script lang="ts" setup>
+import { SearchField } from '@mindenit/ui'
+import { users as mockUsers } from '~/assets/mockDataUsers'
+
+definePageMeta({
+  layout: 'main',
+})
+
+const searchQuery = ref('')
+const itemsPerPage = 10
+const currentPage = ref(1)
+const displayedUsers = ref(mockUsers.slice(0, itemsPerPage))
+
+const filteredUsers = computed(() => {
+  if (!searchQuery.value) {
+    return mockUsers
+  }
+  const lowerCaseQuery = searchQuery.value.toLowerCase()
+  return mockUsers.filter((user) => {
+    return (
+      user.name.toLowerCase().includes(lowerCaseQuery) ||
+      user.username.toLowerCase().includes(lowerCaseQuery) ||
+      user.description.toLowerCase().includes(lowerCaseQuery)
+    )
+  })
+})
+
+watch(filteredUsers, () => {
+  currentPage.value = 1
+  displayedUsers.value = filteredUsers.value.slice(0, itemsPerPage)
+})
+
+const loadMore = () => {
+  console.log('Loading more users...')
+  const nextPageUsers = filteredUsers.value.slice(
+    currentPage.value * itemsPerPage,
+    (currentPage.value + 1) * itemsPerPage,
+  )
+  console.log('New users:', nextPageUsers)
+  displayedUsers.value = [...displayedUsers.value, ...nextPageUsers]
+  currentPage.value++
+}
+
+const observer = ref<IntersectionObserver | null>(null)
+
+onMounted(() => {
+  observer.value = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      loadMore()
+    }
+  })
+
+  const sentinel = document.querySelector('#scroll-sentinel')
+  if (sentinel) {
+    observer.value.observe(sentinel)
+  }
+})
+
+onBeforeUnmount(() => {
+  observer.value?.disconnect()
+})
+</script>
+
+<template>
+  <main class="container flex h-full w-full flex-col items-center text-center">
+    <div class="w-full py-6">
+      <SearchField
+        v-model:modelValue="searchQuery"
+        placeholder="Пошук працює за іменем, юзернеймом або описом профіля..."
+      />
+    </div>
+    <div
+      class="grid w-full gap-4 pb-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3"
+    >
+      <UserCard
+        v-for="user in displayedUsers"
+        :key="user.id"
+        :name="user.name"
+        :username="user.username"
+        :avatarUrl="user.avatar"
+        :description="user.description"
+        :stars="user.stars"
+      />
+    </div>
+    <div id="scroll-sentinel" class="h-4 w-full"></div>
+  </main>
+</template>
